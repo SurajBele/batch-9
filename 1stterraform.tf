cc92207b69f0e490fd700eba95924f21bef79e04
suraj <surajbele51@gmail.com>
2025-05-10 09:04:18 +0530

    terraform file

--- 1stterraform.tf
+++ 1stterraform.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0f88e80871fd81e91"
   instance_type = "t2.micro"
   key_name = "id_rsa"
+  vpc_security_group_ids = ["sg-0d34c3d2b6fa6492a"]
 
   tags = {
     Name = "HelloWorld"
