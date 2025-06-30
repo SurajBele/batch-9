c88e50a1c39f67bf44c4ffd5af28abe107d670aa
suraj <surajbele51@gmail.com>
2025-06-30 22:20:51 +0530

    --- Auto Git Commit ---

--- jenkins/sample.groovy
+++ jenkins/sample.groovy
@@ -5,7 +5,7 @@ pipeline {
     stages {
         stage('pull') {
             steps {
-               git 'https://github.com/SurajBele/studentdata.git'
+                git 'https://github.com/SurajBele/studentdata.git'
                 echo "pulling successful"
             }
         }
