e871f84205fd52100d78aaffd556d29d085225c5
suraj <surajbele51@gmail.com>
2025-07-01 08:37:14 +0530

    --- Auto Git Commit ---

--- jenkins/sample.groovy
+++ jenkins/sample.groovy
@@ -14,8 +14,7 @@ pipeline {
                 sh 'export MAVEN_HOME=/opt/apache-maven-3.9.10
                     export MAVEN_HOME
                     PATH=$PATH:$MAVEN_HOME/bin
-
-                mvn clean package'
+                    mvn clean package'
                 echo "building successful"
             }
         }
