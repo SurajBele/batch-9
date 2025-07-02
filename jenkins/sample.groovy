c7ee4af8d351c0981bbe49a633bb79352e480e3a
suraj <surajbele51@gmail.com>
2025-07-02 08:47:53 +0530

    --- Auto Git Commit ---

--- jenkins/sample.groovy
+++ jenkins/sample.groovy
@@ -23,7 +23,7 @@ pipeline {
         }
         stage('Test') {
             steps {
-                withSonarQubeEnv(insacredentialsId: 'sonar-token') {
+                withSonarQubeEnv(insatallation name: 'sonar-server' credentialsId: 'sonar-token') {
                   mvn clean verify sonar:sonar -Dsonar.projectKey=student
                 }
                 echo "testing successful"
